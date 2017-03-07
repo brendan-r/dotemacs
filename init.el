ecbf6cdf06eb9a94683c979cb89abae9c14cdce7
Brendan Rocks <brendan@brendanrocks.com>
2017-03-06 23:15:32 -0800

    Remove magit whoops

--- init.el
+++ init.el
@@ -526,28 +526,6 @@
 
 
 
-<<<<<<< HEAD
-=======
-;; You do not understand what this does and it's pissing you off ---------------
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (smooth-scrolling smooth-scroll yasnippet eval-in-repl))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
-
->>>>>>> 23d142e7ae61f984e554e869386bce092c393e96
 ;; Appearance ------------------------------------------------------------------
 
 ;; Make emacs transparent
