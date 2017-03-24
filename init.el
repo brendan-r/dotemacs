3b3123ee9347e2f7aa290c3b6fbef2722b38b47e
Brendan Rocks <brendan@brendanrocks.com>
2017-03-24 13:55:44 -0700

    [comint] Some settings you don't understand which sound beneficial
    (Possibly WRT accidentally resubmitting commands in the R buffer all the time)

--- init.el
+++ init.el
@@ -203,6 +203,30 @@
 (define-key comint-mode-map (kbd "<down>")
   'comint-next-input)
 
+;; Possibly make comint buffers more well-behaved when throwing text around
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+
+
+;; Automatically adjust output width in commint buffers
+;; from http://stackoverflow.com/a/11255996/7237812
+(defun comint-fix-window-size ()
+  "Change process window size."
+  (when (derived-mode-p 'comint-mode)
+    (let ((process (get-buffer-process (current-buffer))))
+      (unless (eq nil process)
+        (set-process-window-size process (window-height) (window-width))))))
+
+(defun my-shell-mode-hook ()
+  ;; add this hook as buffer local, so it runs once per window.
+  (add-hook 'window-configuration-change-hook 'comint-fix-window-size nil t))
+
+(add-hook 'shell-mode-hook
+          (lambda()
+             ;; add this hook as buffer local, so it runs once per window.
+             (add-hook 'window-configuration-change-hook 'comint-fix-window-size nil t)))
+
 
 ;; GUI stuff -------------------------------------------------------------------
 
