14083fcf4386fdd3ac054fb57dcdf09ebf727960
Brendan Rocks <brendan@brendanrocks.com>
2017-03-24 13:53:59 -0700

    Vague memories of not having this around causing problems
    On one computer, maybe, I think

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
 ;; Allow packages to be installed ----------------------------------------------
 
 (require 'package)
+(require 'cl)
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
