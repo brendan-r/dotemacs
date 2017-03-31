3580b4d10e416466f78c0dcf788001c41510987a
Brendan Rocks <brendan@brendanrocks.com>
2017-03-31 14:35:46 -0700

    [elsip] Add some REPL settings

--- init.el
+++ init.el
@@ -610,6 +610,37 @@
 (venv-initialize-interactive-shells)
 
 
+
+;; Elisp -----------------------------------------------------------------------
+;; Note: This is straight-up stolen from izahn's dotfiles at
+;; https://github.com/izahn/dotemacs/blob/496c502aad691deaed1076318e92035d229cfa40/init.el#L486
+
+(with-eval-after-load "elisp-mode"
+  (require 'company-elisp)
+  ;; ielm
+  (require 'eval-in-repl-ielm)
+  ;; For .el files
+  (define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eir-eval-in-ielm)
+  (define-key emacs-lisp-mode-map (kbd "<C-return>") 'eir-eval-in-ielm)
+  (define-key emacs-lisp-mode-map (kbd "C-c C-b") 'eval-buffer)
+  (define-key emacs-lisp-mode-map (kbd "<C-S-return>") 'eval-buffer)
+  ;; For *scratch*
+  (define-key lisp-interaction-mode-map "\C-c\C-c" 'eir-eval-in-ielm)
+  (define-key lisp-interaction-mode-map (kbd "<C-return>") 'eir-eval-in-ielm)
+  (define-key lisp-interaction-mode-map (kbd "C-c C-b") 'eval-buffer)
+  (define-key lisp-interaction-mode-map (kbd "<C-S-return>") 'eval-buffer)
+  ;; For M-x info
+  (define-key Info-mode-map (kbd "C-c C-c") 'eir-eval-in-ielm)
+  ;; Set up completions
+  (add-hook 'emacs-lisp-mode-hook
+            (lambda()
+              ;; make sure completion calls company-elisp first
+              (require 'company-elisp)
+              (setq-local company-backends
+                          (delete-dups (cons 'company-elisp (cons 'company-files company-backends)))))))
+
+
+
 ;; R and ESS -------------------------------------------------------------------
 
 (require 'ess-site)
