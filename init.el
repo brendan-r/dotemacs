7ec5cb9b868d7c183f584ea7a2b53730ece3af7f
Brendan Rocks <brendan@brendanrocks.com>
2017-05-14 16:15:15 -0700

    Depend on GNU Emacs >= 25

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
-(when (< emacs-major-version 24)
+(when (< emacs-major-version 25)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
