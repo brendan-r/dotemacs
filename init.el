92f17719035cc68de1005421deb633df46a01598
Brendan Rocks <brendan@brendanrocks.com>
2017-05-14 14:59:12 -0700

    [ess] Aborted attempt to include R buffer commands in REPL history

--- init.el
+++ init.el
@@ -731,6 +731,54 @@
             (run-hooks 'prog-mode-hook)
             ))
 
+;; The iESS R buffer only seems to be able to cycle through things directly
+;; typed into it (e.g. not sent from and R buffer), which makes it pretty
+;; useless. The commands below are an attempt to get the keybindings working for
+;; commands *either* typed or sent from and R buffer
+;;
+;; From: https://stackoverflow.com/a/27440051
+
+;; (defun ess-readline ()
+;;   "Move to previous command entered from script *or* R-process and copy
+;;    to prompt for execution or editing"
+;;   (interactive)
+;;   ;; See how many times function was called
+;;   (if (eq last-command 'ess-readline)
+;;       (setq ess-readline-count (1+ ess-readline-count))
+;;     (setq ess-readline-count 1))
+;;   ;; Move to prompt and delete current input
+;;   (comint-goto-process-mark)
+;;   (end-of-buffer nil) ;; tweak here
+;;   (comint-kill-input)
+;;   ;; Copy n'th command in history where n = ess-readline-count
+;;   (comint-previous-prompt ess-readline-count)
+;;   (comint-copy-old-input)
+;;   ;; Below is needed to update counter for sequential calls
+;;   (setq this-command 'ess-readline)
+;; )
+
+
+;; (defun ess-readnextline ()
+;;   "Move to next command after the one currently copied to prompt and copy
+;;    to prompt for execution or editing"
+;;   (interactive)
+;;   ;; Move to prompt and delete current input
+;;   (comint-goto-process-mark)
+;;   (end-of-buffer nil)
+;;   (comint-kill-input)
+;;   ;; Copy (n - 1)'th command in history where n = ess-readline-count
+;;   (setq ess-readline-count (max 0 (1- ess-readline-count)))
+;;   (when (> ess-readline-count 0)
+;;       (comint-previous-prompt ess-readline-count)
+;;   (comint-copy-old-input))
+;;   ;; Update counter for sequential calls
+;;   (setq this-command 'ess-readline)
+;;   )
+
+;; ;; Define them in the ESS REPL only
+;; (define-key inferior-ess-mode-map (kbd "<up>") 'ess-readline)
+;; (define-key inferior-ess-mode-map (kbd "<down>") 'ess-readnextline)
+
 
 
 ;; Knitr / Rmarkdown -----------------------------------------------------------
