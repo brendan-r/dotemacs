4952aeda05f75043dcad2d71f9bd7c3f46bcca2d
Brendan Rocks <brendan@brendanrocks.com>
2017-05-14 14:57:40 -0700

    [custom.el] Custom faces for markdown-mode and rainbow-delimiters

---
+++ custom.el
@@ -0,0 +1,25 @@
+;; Put user configuration here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (let-alist ess))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-blockquote-face ((t (:inherit font-lock-doc-face :slant italic))))
+ '(markdown-bold-face ((t (:inherit font-lock-string-face :slant normal :weight bold))))
+ '(markdown-header-face ((t (:foreground "#de935f" :weight bold))))
+ '(markdown-header-face-1 ((t (:inherit font-lock-warning-face :weight bold))))
+ '(markdown-header-face-2 ((t (:inherit font-lock-variable-name-face :slant italic :weight bold))))
+ '(markdown-header-face-3 ((t (:inherit font-lock-function-name-face :slant italic :weight bold))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face-1 :slant italic :weight normal))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face-2 :weight normal))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face-3 :weight normal))))
+ '(markdown-italic-face ((t (:inherit font-lock-string-face :slant italic :weight normal))))
+ '(markdown-strike-through-face ((t (:inherit font-lock-string-face :strike-through t))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#66cccc"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#cccccc")))))
