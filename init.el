1f851ff2cbf42a82c20cc4a0d56efb26628e538b
Brendan Rocks <brendan@brendanrocks.com>
2017-05-14 16:28:24 -0700

    Remove unnecessary custom stuff (now in separate file)

--- init.el
+++ init.el
@@ -952,23 +952,6 @@ send regions above point."
 ;; (require 'persp-mode)
 ;; (persp-mode 1)
 
-;; You do not understand what this does and it's pissing you off ---------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ess))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
 ;; save settings made using the customize interface to a sparate file
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (unless (file-exists-p custom-file)
