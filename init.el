776435f8750fe4c5f697100c04e8f2a809fcdd2f
Brendan Rocks <brendan@brendanrocks.com>
2017-08-21 12:34:43 -0700

    [persp-mode] Fix for use with olivetti
    persp-mode issue #74

--- init.el
+++ init.el
@@ -953,6 +953,35 @@ send regions above point."
 ;; (require 'persp-mode)
 ;; (persp-mode 1)
 
+(require 'persp-mode)
+(persp-mode 1)
+
+
+(with-eval-after-load "olivetti"
+  (with-eval-after-load "persp-mode"
+    (defvar persp-olivetti-buffers-backup nil)
+    (add-hook 'persp-before-deactivate-functions
+              #'(lambda (fow)
+                  (dolist (b (mapcar #'window-buffer
+                                     (window-list (selected-frame)
+                                                  'no-minibuf)))
+                    (with-current-buffer b
+                      (when (eq 'olivetti-split-window-sensibly
+                                split-window-preferred-function)
+                        (push b persp-olivetti-buffers-backup)
+                        (remove-hook 'window-configuration-change-hook
+                                     #'olivetti-set-environment t)
+                        (setq-local split-window-preferred-function nil)
+                        (olivetti-reset-all-windows))))))
+    (add-hook 'persp-activated-functions
+              #'(lambda (fow)
+                  (dolist (b persp-olivetti-buffers-backup)
+                    (with-current-buffer b
+                      (setq-local split-window-preferred-function
+                                  'olivetti-split-window-sensibly)
+                      (add-hook 'window-configuration-change-hook
+                                #'olivetti-set-environment nil t)))
+                  (setq persp-olivetti-buffers-backup nil)))))
 
 
 ;; Customization ---------------------------------------------------------------
