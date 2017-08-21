e96f85c911eb58d96ea07fe4498cde284c127c35
Brendan Rocks <brendan@brendanrocks.com>
2017-08-21 12:30:17 -0700

    [themes] Make a few 'safe'
    You tried these out, but didn't like them that much

--- custom.el
+++ custom.el
@@ -5,6 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(create-lockfiles nil)
+ '(custom-safe-themes
+   (quote
+    ("6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(olivetti-body-width 83)
  '(package-selected-packages (quote (edbi ivy polymode let-alist ess))))
 (custom-set-faces
