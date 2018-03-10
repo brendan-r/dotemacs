bb2fd3689af535f6af69778f9ac68ed789ee2849
Brendan Rocks <brendan@brendanrocks.com>
2018-03-09 16:39:17 -0800

    [org] Minor fixes

--- init.el
+++ init.el
@@ -594,16 +594,14 @@
 
 
 
-;; org-mode --------------------------------------------------------------------
-
+;; Org-mode --------------------------------------------------------------------
 
 
 ;; Allow Control-Shift-Up to select wrapped paragraphs
 (setq org-disputed-keys t)
 (add-hook 'org-mode-hook
           (lambda ()
-
-            (define-key org-mode-map (kbd "<M-Prior>") 'org-todo)
+            (define-key org-mode-map (kbd "<M-prior>") 'org-todo)
             ;; (define-key org-mode-map (kbd "<C-S-up>") 'org-backward-paragraph)
             ;; (define-key org-mode-map (kbd "<C-S-down>") 'org-foreward-paragraph)
             ))
