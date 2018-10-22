e1acd47382e8c97e3290ace76731f1c243926a72
Brendan Rocks <brendan@brendanrocks.com>
2018-10-21 17:57:10 -0700

    [nav] Change C-backspace/delete to just delete, not kill words
    This was getting too confusing relative to other apps

--- init.el
+++ init.el
@@ -272,6 +272,28 @@
 (global-set-key (kbd "C-S-z") 'redo) ;; Make ctrl-Z redo
 
 
+
+;; Editing text
+
+(defun delete-word (arg)
+  "Delete characters forward until encountering the end of a word.
+With argument, do this that many times."
+  (interactive "p")
+  (if (use-region-p)
+      (delete-region (region-beginning) (region-end))
+    (delete-region (point) (progn (forward-word arg) (point)))))
+
+(defun backward-delete-word (arg)
+  "Delete characters backward until encountering the end of a word.
+With argument, do this that many times."
+  (interactive "p")
+  (delete-word (- arg)))
+
+(global-set-key (kbd "<C-backspace>") 'backward-delete-word)
+(global-set-key (kbd "<C-delete>") 'delete-word)
+
+
+
 ;; Mouse bindings --------------------------------------------------------------
 
 ;; Mouse bindings
